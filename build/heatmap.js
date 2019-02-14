acbc512e1f57141eaf333cc8a00cd7793fd16021
ivan silvio <ivan.sl@hotmail.com>
2019-02-14 10:44:14 -0200

    Update heatmap.js

--- build/heatmap.js
+++ build/heatmap.js
@@ -24,7 +24,7 @@ var HeatmapConfig = {
   defaultRadius: 40,
   defaultRenderer: 'canvas2d',
   defaultGradient: { 0.25: "rgb(0,0,255)", 0.55: "rgb(0,255,0)", 0.85: "yellow", 1.0: "rgb(255,0,0)"},
-  defaultMaxOpacity: 1,
+  defaultMaxOpacity: 6,
   defaultMinOpacity: 0,
   defaultBlur: .85,
   defaultXField: 'x',
